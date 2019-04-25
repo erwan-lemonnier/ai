62a19612da22851e7dcba47be11a0fdaf600ed35
Erwan Lemonnier <erwan@lemonnier.se>
2019-04-25 18:26:44 +0200

    Scripts to merge unlabelled image pairs into 1 new image

---
+++ merge_images.py
@@ -0,0 +1,37 @@
+#!/usr/local/bin/python3
+
+from PIL import Image
+import logging
+import click
+
+
+@click.command()
+@click.option('--input', nargs=2, type=str, help="Path to 2 images to merge")
+@click.option('--output', nargs=1, type=str, help="Path to the image to create")
+def main(input, output):
+    """Merge two pictures into one"""
+
+    assert len(input) == 2, "You must provide 2 image paths (--help for help)"
+    assert output, "You must provide an output path (--help for help)"
+
+    images = [Image.open(i) for i in input]
+    print("Images is %s" % images)
+    widths, heights = zip(*(i.size for i in images))
+
+    total_width = sum(widths)
+    max_height = max(heights)
+
+    new_im = Image.new('RGB', (total_width, max_height), color='white')
+
+    x_offset = 0
+    for im in images:
+      print("Adding %s to new image" % im)
+      new_im.paste(im, (x_offset, 0))
+      x_offset += im.size[0]
+
+    print("Saving %s" % output)
+    new_im.save(output)
+
+
+if __name__ == "__main__":
+    main()
---
+++ pairs_to_images.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PATH_PAIRS=/Users/erwan/exjobb/unlabelled_pairs
+PATH_MERGED=/Users/erwan/exjobb/merged_pairs
+
+mkdir -p $PATH_MERGED
+
+for DIR in $(ls $PATH_PAIRS); do
+    IMAGE0=$(ls $PATH_PAIRS/$DIR/*.png | head -1)
+    IMAGE1=$(ls $PATH_PAIRS/$DIR/*.png | tail -1)
+
+    # Just a defensive check that both image names are different
+    if [ "$IMAGE0" == "$IMAGE1" ]; then
+        echo "ERROR in $DIR!!! [$IMAGE0] [$IMAGE1]"
+        exit 1
+    fi
+
+    # Generate a new image made of concatenating those two images
+    ./merge_images --input $IMAGE0 $IMAGE1 --output $PATH_MERGED/$DIR.png
+done
